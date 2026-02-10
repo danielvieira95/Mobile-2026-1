9b92a725c83d4521b44ef3bdf3cb03cdcae07287
daniel vieira <engdanielvieira95@gmail.com>
2026-02-09 21:43:01 -0300

    aula 02 TA
    Exemplo aplicativo contador

---
+++ TurmaA/aula02/appaula02/android/app/src/main/kotlin/com/exampleaula02appta/appaula02/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.exampleaula02appta.appaula02
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ TurmaA/aula02/appaula02a/android/app/src/main/kotlin/com/exampleaula02appta/appaula02a/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.exampleaula02appta.appaula02a
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
