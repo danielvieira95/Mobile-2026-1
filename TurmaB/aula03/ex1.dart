0ec46d3f7aa5c9ea8eb9f4582d7522fc239b19cc
daniel vieira <engdanielvieira95@gmail.com>
2026-02-19 19:49:14 -0300

    aula 03
     Exemplos linguagem Dart

---
+++ TurmaB/aula03/ex1.dart
@@ -0,0 +1,12 @@
+// Exemplo 1 linguagem Dart
+// Biblioteca que permite que o usuario digite valores utilizando o teclado
+
+import 'dart:io';
+// Função principal do codigo
+void main(){
+ 
+ print("Digite seu nome"); // exibe mensagem na tela
+ String nome = stdin.readLineSync()!; // Cria variavel nome para armazenar o que o usuario digita e converte para String
+ print("Olá $nome !"); // Exibe a mensagem na tela acessando o valor da variavel
+
+}
