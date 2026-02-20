24fc78203c413db2289ff633bfd31daffa69635e
daniel vieira <engdanielvieira95@gmail.com>
2026-02-19 22:01:00 -0300

    aula 03
    exemplos função , função com retorno, função com parametro obrigatorio e nomeado

---
+++ TurmaB/aula03/ex8.dart
@@ -0,0 +1,11 @@
+// Exemplo função sem retorno
+// 19/02/2026
+// Função do tipo void chamada saudar com o parametro nome
+void saudar(String nome){
+  print('Olá, $nome ! Bem vindo ao Dart');
+}
+
+// Função principal
+void main(){
+  saudar('Daniel');
+}
---
+++ TurmaB/aula03/ex9.dart
@@ -0,0 +1,14 @@
+/*
+Função com retorno do tipo inteiro
+19/02/2026
+*/
+
+// Função com retorno
+int somar(int a, int b){
+  return a+b;
+}
+
+void main(){
+  int resultado = somar(4,5);
+  print('A soma é: $resultado');
+}
